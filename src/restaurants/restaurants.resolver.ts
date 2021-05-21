b4512d482fa5e74ef2031fc4704be34f7526bb14
popawaw <gkwhddbs77@knou.ac.kr>
2021-05-22 08:43:19 +0900

    Nestjs Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  // graphql 에서 list를 명시하는방법
+  @Query((returns) => [Restaurant])
+  // typescript 에서 list를 명시하는 방법
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
