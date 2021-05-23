66502281e6f1565a7f49a3629d8af10cc50aa7e4
popawaw <gkwhddbs77@knou.ac.kr>
2021-05-23 20:30:30 +0900

    nestjs Config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -6,14 +7,20 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
   // Graphql 의 root 모듈을 설정
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      // production 환경일 때는 configModule이 환경변수 파일을 무시합니다.
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'jongyunha',
-      // password: '1234' localhost인 경우는 비밀번호를 생략해도 된다.
-      database: 'nuber-eats',
-      // typeorm 이 db에 연결할때 데이터베이스를 모듈의 현재 상태로 마이그레이션 한다는뜻
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // typeorm 이 db에 연결할때 데이터베이스를 모듈의 현재 상태로 마이그레이션 한다는
       synchronize: true,
       logging: true,
     }),
