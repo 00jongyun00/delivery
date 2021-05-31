4477cc6d8ad083957b7e47fd55d51ea94d0e22d7
popawaw <gkwhddbs77@knou.ac.kr>
2021-05-31 16:49:10 +0900

    Update Restaurant part one

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -2,26 +2,15 @@
 // inputType - 그저 하나의 object 입니다. argument 로써 graphql에 전달하기 위한 용도
 // ArgsType - 분리된 값들을 GraphQL argument로 전달해 줄 수 있도록 해줌
 
-import { InputType, Field, ArgsType } from '@nestjs/graphql';
+import { InputType, Field, ArgsType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurant.entity';
 
-@ArgsType()
-export class CreateRestaurantDto {
-  // @Args('name') name: string,
-  // @Args('isVegan') isVegan: boolean,
-  // @Args('address') address: string,
-  // @Args('ownerName') ownerName: string,
-  @Field((type) => String)
-  @IsString()
-  @Length(5, 10)
-  name: string;
-  @Field((type) => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
-  @Field((type) => String)
-  @IsString()
-  address: string;
-  @Field((type) => String)
-  @IsString()
-  ownerName: string;
-}
+@InputType()
+// OmitType 은 InputType 에서만 작동합니다.
+// Object타입을 변경하기 위해 3번째 인자로 InputType 을 넣어줍니다.
+export class CreateRestaurantDto extends OmitType(
+  Restaurant,
+  ['id'],
+  InputType,
+) {}
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,34 +1,43 @@
 // entity 란 데이터 베이스 모델을 생각하면 이해하기 쉽다.
 
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 // 아직 데이터베이스가 없어서 이해하기 쉽게 특징을 살펴본후 데이터 베이스를 추가할 예정
 @ObjectType()
 @Entity()
 export class Restaurant {
   // Restaurant 를 위한 ObjectType 을 만들어 줍니다.
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   @Field((type) => Number)
   id: number;
+
   @Field((type) => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, { defaultValue: true })
+  @Column({ default: true })
+  @IsBoolean()
+  @IsOptional()
   isVegan: boolean;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   ownerName: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
 
