7a3ddbce2ea22479b7306ec3d056a567d6ef436f
popawaw <gkwhddbs77@knou.ac.kr>
2021-06-05 14:14:41 +0900

    An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,17 +14,18 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
         // make error
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
       //make error
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
