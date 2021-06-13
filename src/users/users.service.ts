a7389fc4b3896fd1894383cd125d52037ee440df
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-06-13 17:50:36 +0900

    Creating Verification
    email verification use to uuid

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,11 +7,14 @@ import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verification: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -26,7 +29,15 @@ export class UsersService {
         // make error
         return { ok: false, error: 'There is a user with that email already' };
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verification.save(
+        this.verification.create({
+          code: 'lalala',
+          user,
+        }),
+      );
       return { ok: true };
     } catch (e) {
       //make error
@@ -74,6 +85,8 @@ export class UsersService {
     const user = await this.users.findOne({ id: userId });
     if (email) {
       user.email = email;
+      user.verified = false;
+      await this.verification.save(this.verification.create({ user }));
     }
     if (password) {
       user.password = password;
