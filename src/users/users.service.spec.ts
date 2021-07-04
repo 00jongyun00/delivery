ee5cf6b20ce8ee9a6bd1dbf4b57f83bbdf2f5e60
jongyunha <popawaw@naver.com>
2021-07-04 15:06:32 +0900

    Unit testing Mocking

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,11 +1,51 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+};
+
+const mockJwtSerivce = {
+  sign: jest.fn(),
+  verifiy: jest.fn(),
+};
+
+const mockMailSerivce = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UserService;
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          // typeorm 에서 불러오지 않고 mock Repository를 제공할예정
+          // mocking = fake function
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtSerivce,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailSerivce,
+        },
+      ],
     }).compile();
     service = module.get<UserService>(UserService);
   });
